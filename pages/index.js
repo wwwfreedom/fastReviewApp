679eb161c99cdf61a1183f4cf5345230298e3e60
Kevin <tig3r86@gmail.com>
2020-11-24 21:05:41 +1100

    signin will save user to firestore

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Review App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Fast Review App</h1>
-        <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
         <div>{auth?.user?.email}</div>
-        {auth?.user && (
+        {auth.user ? (
           <button onClick={(e) => auth.signout()}>Sign Out</button>
+        ) : (
+          <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
         )}
       </main>
 
