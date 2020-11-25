a995f13d53db4cfea2fdfbf5df72de87fd01e505
Kevin <tig3r86@gmail.com>
2020-11-25 16:57:03 +1100

    add global default style override

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,40 @@ import Head from "next/head";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AuthProvider } from "@/lib/auth";
 import themeOverride from "@/styles/theme";
+import { css, Global } from "@emotion/react";
 
 const theme = extendTheme(themeOverride);
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <title>Fast Review App</title>
+      </Head>
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Fast Review App</title>
-      </Head>
       <ChakraProvider theme={theme}>
         <AuthProvider>
+          <GlobalStyle />
           <Component {...pageProps} />
         </AuthProvider>
       </ChakraProvider>
