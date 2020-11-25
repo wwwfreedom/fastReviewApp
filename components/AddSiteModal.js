c2e7034a727cbc03cfbacc73460f09aa8b4b2abf
Kevin <tig3r86@gmail.com>
2020-11-26 10:33:26 +1100

    add react-hook-form to addModalSite

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -9,15 +9,21 @@ import {
   FormControl,
   Input,
   FormLabel,
-  Button
+  Button,
+  Text
 } from "@chakra-ui/react";
 import { useRef } from "react";
+import { useForm } from "react-hook-form";
 const { useDisclosure } = require("@chakra-ui/react");
 
 export default function AddSiteModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
   const initialRef = useRef();
+  const { register, handleSubmit, errors } = useForm();
+
+  const createSite = (value) => {
+    console.log(value);
+  };
 
   return (
     <>
@@ -27,18 +33,37 @@ export default function AddSiteModal() {
 
       <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent as="form" onSubmit={handleSubmit(createSite)}>
           <ModalHeader fontWeight="bold">Add Site</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Name</FormLabel>
-              <Input ref={initialRef} placeholder="Name" />
+              <Input
+                name="site"
+                ref={initialRef}
+                placeholder="Name"
+                ref={register({ required: true })}
+              />
+              {errors.site && (
+                <Text mt={1} fontSize="xs" color="red.500">
+                  Name field is required
+                </Text>
+              )}
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Link</FormLabel>
-              <Input placeholder="https://website.com" />
+              <Input
+                placeholder="https://website.com"
+                ref={register({ required: true })}
+                name="url"
+              />
+              {errors.url && (
+                <Text mt={1} fontSize="xs" color="red.500">
+                  Link field is required
+                </Text>
+              )}
             </FormControl>
           </ModalBody>
 
@@ -46,7 +71,12 @@ export default function AddSiteModal() {
             <Button onClick={onClose} mr={3} fontWeight="medium">
               Cancel
             </Button>
-            <Button background="#99FFFE" color="#194D4C" fontWeight="medium">
+            <Button
+              background="#99FFFE"
+              color="#194D4C"
+              fontWeight="medium"
+              type="submit"
+            >
               Create
             </Button>
           </ModalFooter>
