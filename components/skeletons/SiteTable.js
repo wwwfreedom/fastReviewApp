e9703dad772378258a6ea7e447997a14f5a08fb5
Kevin <tig3r86@gmail.com>
2020-11-27 08:19:48 +1100

    Fix table skeleton on mobile

--- components/skeletons/SiteTable.js
+++ components/skeletons/SiteTable.js
@@ -4,16 +4,16 @@ import { Table, Tr, Th, Td } from "../Table";
 const SkeletonRow = ({ width }) => (
   <Box as="tr">
     <Td>
-      <Skeleton height="10px" w={width} my={4} />
+      <Skeleton height="10px" my={4} />
     </Td>
     <Td>
-      <Skeleton height="10px" w={width} my={4} />
+      <Skeleton height="10px" my={4} />
     </Td>
     <Td>
-      <Skeleton height="10px" w={width} my={4} />
+      <Skeleton height="10px" my={4} />
     </Td>
     <Td>
-      <Skeleton height="10px" w={width} my={4} />
+      <Skeleton height="10px" my={4} />
     </Td>
   </Box>
 );
@@ -27,15 +27,14 @@ const SiteTableSkeleton = () => {
           <Th>Site Link</Th>
           <Th>Feedback Link</Th>
           <Th>Date Added</Th>
-          <Th>{""}</Th>
         </Tr>
       </thead>
       <tbody>
-        <SkeletonRow width="75px" />
-        <SkeletonRow width="125px" />
-        <SkeletonRow width="50px" />
-        <SkeletonRow width="100px" />
-        <SkeletonRow width="75px" />
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
       </tbody>
     </Table>
   );
