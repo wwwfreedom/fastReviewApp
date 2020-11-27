145023ce15d845bbaacfd1b772c709cbe6a5f4b0
Kevin <tig3r86@gmail.com>
2020-11-27 12:56:20 +1100

    add limit orderby desc with format date by dayjs to api/sites, add swr

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,17 +1,24 @@
 import db from "@/lib/firebase_serverside_adminSdk";
+import dayjs from "dayjs";
 
 export default async function handler(_, res) {
   const sites = [];
   const sitesRef = await db.collection("sites");
-  const snapshot = await sitesRef.get();
+  const snapshot = await sitesRef.orderBy("createdAt", "desc").limit(10).get();
   if (snapshot.empty) {
     console.log("No matching documents.");
     return res.status(403).json({ error: "Nothing was found" });
   }
 
   snapshot.forEach((doc) => {
-    sites.push({ id: doc.id, ...doc.data() });
+    const { createdAt, ...data } = doc.data();
+    const formattedCreatedAt = dayjs(createdAt.toDate()).format("DD/MM/YYYY");
+    sites.push({
+      id: doc.id,
+      createdAt: formattedCreatedAt,
+      ...data
+    });
   });
 
-  return res.status(200).json(sites);
+  return res.status(200).json({ sites });
 }
