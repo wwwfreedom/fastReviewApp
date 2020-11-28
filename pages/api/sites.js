cf107e965d0e677249bf760b88ef1a763c95d6c7
Kevin <tig3r86@gmail.com>
2020-11-29 07:51:06 +1100

    handle getAllsites error

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,7 +1,11 @@
 import { getAllSites } from "@/lib/firestoreDb_admin";
 
 export default async function handler(_, res) {
-  const sites = await getAllSites();
+  const { sites, error } = await getAllSites();
+
+  if (error) {
+    return res.status(500).json({ error });
+  }
 
   return res.status(200).json({ sites });
 }
