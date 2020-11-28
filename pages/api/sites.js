ad325591d888c68583cdf6bff28c31f2c18955df
Kevin <tig3r86@gmail.com>
2020-11-28 22:09:33 +1100

    refactor sites api logic

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,24 +1,7 @@
-import db from "@/lib/firebase_serverside_adminSdk";
-import formatDate from "@/utils/formatDate";
+import { getAllSites } from "@/lib/firestoreDb_admin";
 
 export default async function handler(_, res) {
-  const sites = [];
-  const sitesRef = await db.collection("sites");
-  const snapshot = await sitesRef.orderBy("createdAt", "desc").limit(10).get();
-  if (snapshot.empty) {
-    console.log("No matching documents.");
-    return res.status(403).json({ error: "Nothing was found" });
-  }
-
-  snapshot.forEach((doc) => {
-    const { createdAt, ...data } = doc.data();
-    const formattedCreatedAt = formatDate(createdAt.toDate());
-    sites.push({
-      id: doc.id,
-      createdAt: formattedCreatedAt,
-      ...data
-    });
-  });
+  const sites = await getAllSites();
 
   return res.status(200).json({ sites });
 }
