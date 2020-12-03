eab32cbd1d0d6483cf5d7e2e4ec492dcd84cb317
Kevin <tig3r86@gmail.com>
2020-12-03 15:58:54 +1100

    add logger util and logflare setup

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,5 +1,6 @@
 import { auth } from "@/lib/firebase_serverside_adminSdk";
 import { getUserSites } from "@/lib/firestoreDb_admin";
+import { formatObjectKeys, logger } from "@/utils/logger";
 
 export default async function handler(req, res) {
   try {
@@ -7,6 +8,19 @@ export default async function handler(req, res) {
     const sites = await getUserSites(uid);
     return res.status(200).json(sites);
   } catch (error) {
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      error.message
+    );
     return res.status(500).json({ error });
   }
 }
