820aa64f0c5ca4548e5a7d805bfe0c6046b52407
Kevin <tig3r86@gmail.com>
2020-12-05 17:37:37 +1100

    add loading to stripe btn, add logout to account page

--- pages/account.js
+++ pages/account.js
@@ -1,25 +1,23 @@
+import { useState } from "react";
+import { Button, Stack } from "@chakra-ui/react";
 import DashboardShell from "@/components/DashboardShell";
-import { useAuth } from "@/lib/auth";
-import { Box, Button, Stack } from "@chakra-ui/react";
 import { createCheckoutSession, goToBillingPortal } from "@/lib/firestoreDb";
+import { useAuth } from "@/lib/auth";
 
 export default function Account() {
-  const { user } = useAuth();
-  // if (!data) {
-  //   return (
-  //     <DashboardShell>
-  //       <SiteTableHeader />
-  //       <SiteTableSkeleton />
-  //     </DashboardShell>
-  //   );
-  // }
+  const { user, signout } = useAuth();
+  const [isSubscriptionLoading, setIsSubscriptionLoading] = useState(false);
+  const [isBillingLoading, setIsBillingLoading] = useState(false);
 
   return (
     <DashboardShell>
       <Stack direction="row">
         <Button
           size="md"
-          onClick={() => createCheckoutSession(user.uid)}
+          onClick={() => {
+            setIsSubscriptionLoading(true);
+            createCheckoutSession(user.uid);
+          }}
           backgroundColor="gray.900"
           color="white"
           fontWeight="medium"
@@ -28,12 +26,17 @@ export default function Account() {
             bg: "gray.800",
             transform: "scale(0.95)"
           }}
+          isLoading={isSubscriptionLoading}
+          disabled={isBillingLoading}
         >
           Upgrade to starter
         </Button>
         <Button
           size="md"
-          onClick={() => goToBillingPortal()}
+          onClick={() => {
+            setIsBillingLoading(true);
+            goToBillingPortal();
+          }}
           backgroundColor="gray.900"
           color="white"
           fontWeight="medium"
@@ -42,9 +45,12 @@ export default function Account() {
             bg: "gray.800",
             transform: "scale(0.95)"
           }}
+          isLoading={isBillingLoading}
+          disabled={isSubscriptionLoading}
         >
           View billing portal
         </Button>
+        <Button onClick={() => signout()}>Log Out</Button>
       </Stack>
     </DashboardShell>
   );
