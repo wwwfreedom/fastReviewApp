180b816b4cc453f1898ce14d83af546dfb667aca
Kevin <tig3r86@gmail.com>
2020-12-06 23:26:06 +1100

    update feedback optimistically using immer

--- components/FeedbackRow.js
+++ components/FeedbackRow.js
@@ -1,15 +1,33 @@
 import { useAuth } from "@/lib/auth";
 import { updateFeedback } from "@/lib/firestoreDb";
 import { Box, Code, Switch } from "@chakra-ui/react";
+import produce from "immer";
+import { useState } from "react";
 import { mutate } from "swr";
 import RemoveButtonAlert from "./RemoveButton";
 import { Td } from "./Table";
 
 export default function FeedbackRow({ feedback }) {
   const { status, author, text, id } = feedback;
+  const [isChecked, setIsChecked] = useState(status === "active");
   const auth = useAuth();
+
   const toggleFeedback = async () => {
-    const updatedStatus = status === "active" ? "pending" : "active";
+    setIsChecked(!isChecked);
+    const updatedStatus = !isChecked ? "active" : "pending";
+
+    // update feedback status optimistically
+    // refer here for context on immer usage https://sergiodxa.com/articles/swr/mutate-immer
+    mutate(
+      ["/api/feedback", auth.user.token],
+      produce((data) => {
+        const updatedFeedbackIndex = data.feedback.findIndex(
+          (i) => i.id === id
+        );
+        data.feedback[updatedFeedbackIndex].status = updatedStatus;
+      }),
+      false
+    );
     await updateFeedback(id, { status: updatedStatus });
     mutate(["/api/feedback", auth.user.token]);
   };
@@ -25,7 +43,7 @@ export default function FeedbackRow({ feedback }) {
         <Switch
           colorScheme="green"
           onChange={toggleFeedback}
-          isChecked={status === "active"}
+          isChecked={isChecked}
         />
       </Td>
       <Td>
