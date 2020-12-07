ee1e3f0726a404b20ddc269bf0e9d3f488ae7330
Kevin <tig3r86@gmail.com>
2020-12-07 19:25:37 +1100

    add updatedAt, util fn to convert firebase server timeStampt to JS date

---
+++ utils/firebaseServerTimeStampToDateString.js
@@ -0,0 +1,23 @@
+import formatDate from "./formatDate";
+
+// replace the firebase.firestore.FieldValue.serverTimestamp() value in createdAt and updatedAt to normal date string.
+// refer to here https://firebase.google.com/docs/reference/android/com/google/firebase/Timestamp
+export default function firebaseServerTimeStampToDateString(obj) {
+  if (!obj) {
+    throw new Error(
+      "require an object as argument to firebaseServerTimeStampToDateString"
+    );
+  }
+
+  const parsedObj = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (key === "createdAt" || key === "updatedAt") {
+      parsedObj[key] = formatDate(value.toDate());
+    } else {
+      parsedObj[key] = value;
+    }
+  }
+
+  return parsedObj;
+}
