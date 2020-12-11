5772118344f4fe7ae9409f1c9cc25a0376998eb1
Kevin Truong <tig3r86@gmail.com>
2020-12-11 17:06:47 +1100

    format account.js

--- pages/account.js
+++ pages/account.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react'
 import {
   Avatar,
   Heading,
@@ -12,85 +12,85 @@ import {
   StatLabel,
   StatNumber,
   StatHelpText
-} from "@chakra-ui/react";
-import DashboardShell from "@/components/DashboardShell";
-import { goToBillingPortal } from "@/lib/firestoreDb";
-import { useAuth } from "@/lib/auth";
-import Page from "@/components/Page";
+} from '@chakra-ui/react'
+import DashboardShell from '@/components/DashboardShell'
+import { goToBillingPortal } from '@/lib/firestoreDb'
+import { useAuth } from '@/lib/auth'
+import Page from '@/components/Page'
 
 const FeedbackUsage = () => (
   <StatGroup>
     <Stat>
-      <StatLabel color="gray.700">Feedback</StatLabel>
-      <StatNumber fontWeight="medium">∞</StatNumber>
+      <StatLabel color='gray.700'>Feedback</StatLabel>
+      <StatNumber fontWeight='medium'>∞</StatNumber>
       <StatHelpText>10,000 limit</StatHelpText>
     </Stat>
 
     <Stat>
-      <StatLabel color="gray.700">Sites</StatLabel>
-      <StatNumber fontWeight="medium">1/∞</StatNumber>
+      <StatLabel color='gray.700'>Sites</StatLabel>
+      <StatNumber fontWeight='medium'>1/∞</StatNumber>
       <StatHelpText>Unlimited Sites</StatHelpText>
     </Stat>
   </StatGroup>
-);
+)
 
 const SettingsTable = ({ stripeRole, children }) => (
   <Box
-    backgroundColor="white"
+    backgroundColor='white'
     mt={8}
     borderRadius={[0, 8, 8]}
-    boxShadow="0px 4px 10px rgba(0, 0, 0, 0.05)"
+    boxShadow='0px 4px 10px rgba(0, 0, 0, 0.05)'
   >
     <Flex
-      backgroundColor="gray.50"
+      backgroundColor='gray.50'
       borderTopLeftRadius={[0, 8, 8]}
       borderTopRightRadius={[0, 8, 8]}
-      borderBottom="1px solid"
-      borderBottomColor="gray.200"
+      borderBottom='1px solid'
+      borderBottomColor='gray.200'
       px={6}
       py={4}
     >
-      <Flex justify="space-between" align="center" w="full">
+      <Flex justify='space-between' align='center' w='full'>
         <Text
-          textTransform="uppercase"
-          fontSize="xs"
-          color="gray.500"
-          fontWeight="medium"
+          textTransform='uppercase'
+          fontSize='xs'
+          color='gray.500'
+          fontWeight='medium'
           mt={1}
         >
           Settings
         </Text>
-        <Badge h="1rem" colorScheme="blue">
+        <Badge h='1rem' colorScheme='blue'>
           {stripeRole}
         </Badge>
       </Flex>
     </Flex>
-    <Flex direction="column" p={6}>
+    <Flex direction='column' p={6}>
       {children}
     </Flex>
   </Box>
-);
+)
 
-function Account() {
-  const { user, signout } = useAuth();
-  const [isBillingLoading, setIsBillingLoading] = useState(false);
+function Account () {
+  const { user, signout } = useAuth()
+  const [isBillingLoading, setIsBillingLoading] = useState(false)
 
   return (
     <DashboardShell>
       <Flex
-        direction="column"
-        maxW="600px"
-        align={["left", "center"]}
-        margin="0 auto"
+        direction='column'
+        maxW='600px'
+        align={['left', 'center']}
+        margin='0 auto'
       >
-        <Flex direction="column" align={["left", "center"]} ml={4}>
+        <Flex direction='column' align={['left', 'center']} ml={4}>
           <Avatar
-            w={["3rem", "6rem"]}
-            h={["3rem", "6rem"]}
+            w={['3rem', '6rem']}
+            h={['3rem', '6rem']}
             mb={4}
             src={user?.photoUrl}
           />
-          <Heading letterSpacing="-1px">{user?.name}</Heading>
+          <Heading letterSpacing='-1px'>{user?.name}</Heading>
           <Text>{user?.email}</Text>
         </Flex>
         <SettingsTable stripeRole={user?.stripeRole}>
@@ -100,24 +100,24 @@ function Account() {
             subscription. You can also update card information and billing
             addresses through the secure portal.
           </Text>
-          <Flex justify="flex-end">
-            <Button variant="ghost" ml={4} onClick={() => signout()}>
+          <Flex justify='flex-end'>
+            <Button variant='ghost' ml={4} onClick={() => signout()}>
               Log Out
             </Button>
             <Button
               onClick={() => {
-                setIsBillingLoading(true);
-                goToBillingPortal();
+                setIsBillingLoading(true)
+                goToBillingPortal()
               }}
-              backgroundColor="gray.900"
-              color="white"
-              fontWeight="medium"
+              backgroundColor='gray.900'
+              color='white'
+              fontWeight='medium'
               ml={4}
               isLoading={isBillingLoading}
-              _hover={{ bg: "gray.700" }}
+              _hover={{ bg: 'gray.700' }}
               _active={{
-                bg: "gray.800",
-                transform: "scale(0.95)"
+                bg: 'gray.800',
+                transform: 'scale(0.95)'
               }}
             >
               Manage Billing
@@ -126,13 +126,13 @@ function Account() {
         </SettingsTable>
       </Flex>
     </DashboardShell>
-  );
+  )
 }
 
-export default function AccountPage() {
+export default function AccountPage () {
   return (
-    <Page name="Account" path="/account">
+    <Page name='Account' path='/account'>
       <Account />
     </Page>
-  );
+  )
 }
